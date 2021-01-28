886535e05e04f903ec175be23f1750b27f76ac7c
Numchik <maksim.nikitjuk@icloud.com>
2021-01-28 09:41:02 +0500

    finishedProject

--- trafficLightHomeWork/ViewController.swift
+++ trafficLightHomeWork/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
+enum AdmissibleLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var admisibleLight = AdmissibleLight.red
+    
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redView.layer.cornerRadius = 65
+        yellowView.layer.cornerRadius = 65
+        greenView.layer.cornerRadius = 65
+        
+        redView.alpha = lightOff
+        yellowView.alpha = lightOff
+        greenView.alpha = lightOff
     }
 
-
+    @IBAction func startButtonPush() {
+        
+        startButton.setTitle("NEXT", for:.normal)
+        
+        switch admisibleLight{
+        case .red:
+            greenView.alpha = lightOff
+            redView.alpha = lightOn
+            admisibleLight = .yellow
+        case .yellow:
+            redView.alpha = lightOff
+            yellowView.alpha = lightOn
+            admisibleLight = .green
+        case .green:
+            greenView.alpha = lightOn
+            yellowView.alpha = lightOff
+            admisibleLight = .red
+        }
+        
+        
+    }
+    
 }
 
